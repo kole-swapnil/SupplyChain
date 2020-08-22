901e3283f667ed91509831287006a47d65c2c865
kole-swapnil <swapnilkole7500@gmail.com>
2020-08-22 17:31:31 +0530

    Unit Tests added

--- truffle-config.js
+++ truffle-config.js
@@ -9,6 +9,11 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
+    },
+    test: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
     }
   },
   compilers: {
