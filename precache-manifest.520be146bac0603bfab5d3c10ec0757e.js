1fd766eeeab5b8e9c655c0e979a2ff798307a686
Swapnil Kole <swapnilkole7500@gmail.com>
2020-09-09 20:00:45 +0530

    Updates

---
+++ precache-manifest.520be146bac0603bfab5d3c10ec0757e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58158ab9defb74b141c4abc137e6b0d1",
+    "url": "/SupplyChain/index.html"
+  },
+  {
+    "revision": "7c45b96f2dcb5d1455f3",
+    "url": "/SupplyChain/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "0aa04a84a5e442d0c6dc",
+    "url": "/SupplyChain/static/js/2.32152a4b.chunk.js"
+  },
+  {
+    "revision": "7c45b96f2dcb5d1455f3",
+    "url": "/SupplyChain/static/js/main.571b6e91.chunk.js"
+  },
+  {
+    "revision": "9480ecd8dd23446e7b3b",
+    "url": "/SupplyChain/static/js/runtime-main.de6d2efd.js"
+  }
+]);
